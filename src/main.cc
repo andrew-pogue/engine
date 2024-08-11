09ce80366685e111d9e3b8e97a1fe3f922bd4f2d
Andrew Pogue <andrewthomaspogue@gmail.com>
2024-08-10 18:32:15 -0700

    main loop

--- src/main.cc
+++ src/main.cc
@@ -17,7 +17,6 @@ void must_init(bool test, const char *error_message) {
 }
 
 int main(int argc, char **argv) {
-    // must have argc and argv variables, required by allegro 
     (void)argc; (void)argv; // prevent unused variable compiler warning
 
     must_init(al_init(), "Failed to initialize allegro.");
@@ -35,13 +34,19 @@ int main(int argc, char **argv) {
         al_get_keyboard_event_source(),
         al_get_mouse_event_source(),
         display.get_event_source() };
+
     ALLEGRO_EVENT event;
-    bool play = true, update = true, render = true;
+    const double step_size = 1.0/60.0;
+    bool play = true, render = true;
+    double lag = 0.0, time_prior = al_get_time();
 
     while (play) {
-        // handle event
-        while (!event_queue.is_empty()) {
-            event_queue.wait_for_event(event);
+        double time_current = al_get_time(),
+               time_elapsed = time_current - time_prior;
+
+        ///// HANDLE EVENT /////////////////////////////////////////////////
+        
+        while (event_queue.next(event)) {
             display.handle_event(event);
             mouse.handle_event(event);
             keyboard.handle_event(event);
@@ -58,38 +63,40 @@ int main(int argc, char **argv) {
             }
         }
 
-        if (update) {
-            // handle input
-            // world.clear();
-            auto keyboard_input = keyboard.poll();
-            if (keyboard_input[ALLEGRO_KEY_ESCAPE]) play = false;
-            // if (keyboard_input[ALLEGRO_KEY_UP]) player.coord.y++;
-            // if (keyboard_input[ALLEGRO_KEY_DOWN]) player.coord.y--;
-            // if (keyboard_input[ALLEGRO_KEY_RIGHT]) player.coord.x++;
-            // if (keyboard_input[ALLEGRO_KEY_LEFT]) player.coord.x--;
-            
-            // update
-            // update = false;
+        ///// HANDLE INPUT /////////////////////////////////////////////////
+        
+        auto keyboard_input = keyboard.poll();
+        if (keyboard_input[ALLEGRO_KEY_ESCAPE]) play = false;
+        
+        ///// FIXED UPDATE /////////////////////////////////////////////////
+        /// fixed time step for physics and game logic
+        
+        for (lag += time_elapsed; lag >= step_size; lag -= step_size) {
         }
         
-        // render
-        if (render) {
-            al_set_target_backbuffer(display.get());
-            al_clear_to_color(al_map_rgb(0,0,0));
-            al_draw_text(font.get(), al_map_rgb(255,255,255),
-                    display.get_width() / 2.0f, display.get_height() / 2.0f,
-                    ALLEGRO_ALIGN_CENTRE, "HELLO WORLD"); 
-            /*
-            for (auto coord : camera.range()) {
-                coord.x = coord.x % world.width();
-                coord.y = coord.y % world.height();
-                for (auto id : world[coord])
-                    render(camera.translate(coord), id);
-            }
-            */
-            al_flip_display();
-            // render = false;
+        ///// UPDATE //////////////////////////////////////////////////////
+        /// variable time step for gui and visual effects
+
+        ///// RENDER //////////////////////////////////////////////////////
+        
+        al_set_target_backbuffer(display.get());
+        al_clear_to_color(al_map_rgb(0,0,0));
+        al_draw_text(font.get(), al_map_rgb(255,255,255),
+                display.get_width() / 2.0f, display.get_height() / 2.0f,
+                ALLEGRO_ALIGN_CENTRE, "HELLO WORLD"); 
+        /*
+        for (auto coord : camera.range()) {
+            coord.x = coord.x % world.width();
+            coord.y = coord.y % world.height();
+            for (auto id : world[coord])
+                render(camera.translate(coord), id);
         }
+        */
+        al_flip_display();
+        
+        //////////////////////////////////////////////////////////////////
+
+        time_prior = time_current;
     }
 
     return 0;
