1ea36f8f54e6876e5812c7b15b84daedb7f910ab
Andrew Pogue <andrewthomaspogue@gmail.com>
2024-08-15 17:16:29 -0700

    textbox

--- src/main.cc
+++ src/main.cc
@@ -32,25 +32,45 @@ int main(int argc, char **argv) {
     Keyboard keyboard(display.get());
     Mouse mouse(display.get());
     Camera camera({0,0,0}, display.get_width(), display.get_height());
-    Font font{"assets/font/PressStart2P-Regular.ttf", 36};
+    Font font_title{"assets/font/PressStart2P-Regular.ttf", 36};
+    Font font_body{"assets/font/PressStart2P-Regular.ttf", 14};
     EventQueue event_queue{
         al_get_keyboard_event_source(),
         al_get_mouse_event_source(),
         display.get_event_source() };
 
-    TextBox textbox(display.get(), 
-        display.get_width() / 2.0f, display.get_height() / 2.0f,
-        500.0f, 100.0f, "HELLO WORLD BONJOUR LE MONDE", font, al_map_rgb(0,0,0));
-    textbox.border.enabled = true;
-    textbox.border.thickness = 2.0f;
-    textbox.border.color = al_map_rgb(0,155,0);
-    textbox.border.offset = 4.0f;
-    textbox.fill.enabled = true;
-    textbox.fill.color = al_map_rgb(0,155,0);
-    textbox.padding = 4.0f;
-    textbox.align.vertical = Align::CENTER;
-    textbox.align.horizontal = Align::CENTER;
-    textbox.text.flags = ALLEGRO_ALIGN_CENTRE;
+    TextBox title(display.get(), 
+        display.get_width() / 2.0f, 50.0f,
+        500.0f, 50.0f, "HELLO WORLD", font_title, al_map_rgb(0,0,0));
+    title.border.enabled = true;
+    title.border.thickness = 2.0f;
+    title.border.color = al_map_rgb(255,255,255);
+    title.border.padding = 4.0f;
+    title.fill.enabled = true;
+    title.fill.color = al_map_rgb(0,155,0);
+    title.align.vertical = Align::CENTER;
+    title.align.horizontal = Align::CENTER;
+    title.text.padding = 4.0f;
+    title.text.flags = ALLEGRO_ALIGN_CENTRE;
+
+    const char *txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet mauris quis tortor consequat maximus a sit amet est. Aliquam ullamcorper lorem eu nunc ultrices sollicitudin. Quisque est nunc, scelerisque pellentesque luctus at, ultricies in lorem. Nam malesuada commodo suscipit. Donec rhoncus, ipsum a condimentum gravida, mauris mauris facilisis magna, id interdum metus felis eget libero. Quisque condimentum risus ut elit consequat semper. Mauris ultrices gravida nibh, a sollicitudin lorem iaculis sed. Integer fringilla accumsan lorem, a rutrum orci blandit sit amet. Donec ultrices libero a ante accumsan, vel semper orci posuere. Curabitur eget urna imperdiet, vulputate neque dapibus, pellentesque nisl. Ut felis mi, ultrices non dui nec, rhoncus efficitur est.\n\nNulla cursus ac enim ut cursus. Vivamus venenatis eros vitae fringilla dapibus. Nunc commodo pharetra diam quis interdum. Sed imperdiet nisi vulputate mi malesuada eleifend. Vivamus iaculis sollicitudin dolor. Phasellus erat urna, elementum in feugiat vitae, feugiat viverra elit. Donec lacinia nulla ut lorem aliquet, ac ultrices quam ultricies. Proin euismod libero et efficitur rutrum. Donec vehicula, nisl in finibus maximus, libero justo consectetur leo, quis auctor quam ex in arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus sit amet eros ultrices, vehicula erat vitae, viverra risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vulputate consectetur malesuada.\n\nVestibulum ac justo viverra, pellentesque ipsum quis, sagittis nibh. Vivamus sed est velit. Cras faucibus leo non leo imperdiet dapibus. Aenean sit amet justo vel magna tempor dictum. Aliquam accumsan varius pellentesque. Etiam suscipit dui nec est consectetur auctor eget eget quam. Nullam tincidunt fringilla nulla, imperdiet porttitor dolor dignissim ut. Aenean rutrum sagittis viverra. Mauris nec tellus eu massa sodales pretium. Curabitur interdum sollicitudin erat at congue.\n\nAenean tristique elit neque. Vivamus feugiat tempus nisl, ut dictum lectus rhoncus at. Cras et tincidunt tellus, id feugiat tellus. Proin aliquam felis at sem tempor dictum. Aliquam rutrum ipsum sapien, in commodo diam lacinia vitae. Aliquam pellentesque sem ex, ac pulvinar nibh pulvinar non. Donec et odio eu arcu pretium euismod vitae ut erat. Donec ut nisi feugiat, convallis sapien id, convallis massa. Nam a odio ante. Integer scelerisque elit at magna iaculis pellentesque. Praesent eu lacinia nisl, a dictum dui. Sed id pellentesque lectus. Pellentesque sed efficitur lorem. Praesent rutrum ex et enim consectetur, a bibendum nisi auctor. Proin at metus porttitor, dictum ex ac, tincidunt odio. Sed quis justo leo.\n\nNunc scelerisque ex tortor, in elementum diam viverra sed. Nullam ut lorem posuere, dignissim sem at, fermentum augue. In aliquet, urna ac ullamcorper laoreet, diam tellus tincidunt sem, et euismod justo sem a est. Mauris suscipit varius vulputate. Etiam ullamcorper velit sit amet nisi varius, ac lobortis nulla lacinia. Ut ultrices pretium urna, ut luctus leo interdum quis. Sed molestie eleifend lorem vel luctus. In id mi non erat tristique fermentum vitae nec elit. Nullam sit amet diam at ante pulvinar lobortis. Vivamus suscipit feugiat ipsum, vel scelerisque dolor rhoncus in. Maecenas sed congue sapien. Quisque finibus ac magna sit amet hendrerit.\n\n";
+
+    TextBox body(display.get(), 
+        display.get_width() / 2.0f, display.get_height() / 2.0f + 25.0f,
+        title.width, display.get_height() - 150.0f,
+        txt, font_body, al_map_rgb(0,0,0));
+    body.border.enabled = true;
+    body.border.thickness = 2.0f;
+    body.border.color = al_map_rgb(0,155,0);
+    body.border.padding = 4.0f;
+    body.fill.enabled = true;
+    body.fill.color = al_map_rgb(0,155,0);
+    body.align.vertical = Align::CENTER;
+    body.align.horizontal = Align::CENTER;
+    body.text.padding = 4.0f;
+    body.text.flags = ALLEGRO_ALIGN_LEFT;
+    body.text.multiline = true;
+    body.editable = true;
 
     ALLEGRO_EVENT event;
     const double step_size = 1.0/60.0;
@@ -67,7 +87,8 @@ int main(int argc, char **argv) {
             display.handle_event(event);
             mouse.handle_event(event);
             keyboard.handle_event(event);
-            textbox.handle_event(event);
+            title.handle_event(event);
+            body.handle_event(event);
             switch (event.type) {
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 if (event.display.source == display) {
@@ -75,10 +96,12 @@ int main(int argc, char **argv) {
                 } break;
             case ALLEGRO_EVENT_DISPLAY_RESIZE:
                 if (event.display.source == display) {
-                    textbox.x = display.get_width() / 2.0f;
-                    textbox.y = display.get_height() / 2.0f;
-                    camera.viewport.width = display.get_width();
-                    camera.viewport.height = display.get_height();
+                    title.x = event.display.width / 2.0f;
+                    body.x = event.display.width / 2.0f;
+                    body.y = event.display.height / 2.0f + 25.0f;
+                    body.height = event.display.height - 150.0f;
+                    camera.viewport.width = event.display.width;
+                    camera.viewport.height = event.display.height;
                 } break;
             }
         }
@@ -101,7 +124,8 @@ int main(int argc, char **argv) {
         
         al_set_target_backbuffer(display.get());
         al_clear_to_color(al_map_rgb(0,0,0));
-        textbox.render();
+        title.render();
+        body.render();
         /*
         for (auto coord : camera.range()) {
             coord.x = coord.x % world.width();
--- src/textbox.hh
+++ src/textbox.hh
@@ -3,13 +3,14 @@
 #include <allegro5/allegro_primitives.h>
 #include "font.hh"
 #include "keyboard.hh"
-// #include <iostream>
 
 struct Text {
     ALLEGRO_USTR *string;
     Font font;
     ALLEGRO_COLOR color;
-    int flags;
+    int flags = 0;
+    float padding = 0.0f, offset = 0.0f;
+    bool multiline = false;
     ~Text() { al_ustr_free(string); }
 };
 
@@ -20,7 +21,7 @@ struct Fill {
 
 struct Border {
     bool enabled;
-    float thickness, offset;
+    float thickness, offset, padding;
     ALLEGRO_COLOR color;
 };
 
@@ -43,13 +44,13 @@ private:
 
     struct DrawLineParams {
         Text &text;
-        float x, y, w, h, scroll;
+        float x, y, w, h;
     };
 
     static bool draw_line(int line_num, const ALLEGRO_USTR *line, void *extra) {
         DrawLineParams params = *(DrawLineParams *)extra;
         const float h = params.text.font.get_line_height(),
-              y = params.y + line_num * h - params.scroll;
+              y = params.y + line_num * h - params.text.offset;
         if (y + h < params.y) return true;
         if (y > params.y + params.h) return false;
         ALLEGRO_USTR_INFO info;
@@ -63,19 +64,28 @@ private:
     }
 
     void draw_multiline_text(float i, float j, float w, float h) {
-        // printf("x:%f y:%f w:%f h:%f\n", i, j, w, h);
-        // al_set_clipping_rectangle(i, j, w, h);
-        if (align.horizontal = Align::CENTER) i += w / 2.0f;
-        DrawLineParams params{text, i, j, w, h, scroll};
+        al_set_clipping_rectangle(i, j, w, h);
+        if (text.flags == ALLEGRO_ALIGN_CENTRE) i += w / 2.0f;
+        DrawLineParams params{text, i, j, w, h};
         al_do_multiline_ustr(text.font.get(), w, text.string, draw_line, &params);
-        // al_reset_clipping_rectangle();
-        al_draw_rounded_rectangle(i, j, i+w, j+h, rx, ry, al_map_rgb(255,255,255), 2.0f); 
+        al_reset_clipping_rectangle();
+    }
+
+    void draw_text(float i, float j, float w, float h) {
+        al_set_clipping_rectangle(i, j, w, h);
+        if (text.flags == ALLEGRO_ALIGN_CENTRE) {
+            i += w / 2.0f;
+            j += h / 2.0f - text.font.get_line_height() / 2.0f;
+        }
+        al_draw_ustr(text.font.get(), text.color, i, j, text.flags, text.string); 
+        al_reset_clipping_rectangle();
     }
 
 public:
     
     ALLEGRO_DISPLAY *display;
-    float x, y, width, height, rx, ry, padding, scroll;
+    float x, y, width, height, rx, ry;
+    bool editable;
 
     Text text;
     Fill fill;
@@ -85,17 +95,18 @@ public:
     TextBox(ALLEGRO_DISPLAY *display, float x, float y, float w, float h, const char *str, const Font &font, ALLEGRO_COLOR color)
         : display(display)
         , x(x), y(y), width(w), height(h)
-        , rx(0.0f), ry(0.0f), padding(0.0f), scroll(0.0f)
-        , text{al_ustr_new(str), font, color, ALLEGRO_ALIGN_LEFT}
+        , rx(0.0f), ry(0.0f)
+        , editable(false)
+        , text{al_ustr_new(str), font, color, ALLEGRO_ALIGN_LEFT, 0.0f, 0.0f, false}
         , fill{false, al_map_rgb(255, 255, 255)}
-        , border{true, 2.0f, 0.0f, al_map_rgb(255, 255, 255)}
+        , border{true, 2.0f, 0.0f, 0.0f, al_map_rgb(255, 255, 255)}
         , align{Align::RIGHT, Align::BOTTOM}
     { }
 
     void handle_event(const ALLEGRO_EVENT &event) {
         switch(event.type) {
         case ALLEGRO_EVENT_KEY_CHAR:
-            if (event.keyboard.display == display && event.keyboard.unichar > 0) {
+            if (event.keyboard.display == display && event.keyboard.unichar > 0 && editable) {
                 switch(event.keyboard.keycode) {
                 case ALLEGRO_KEY_BACKSPACE:
                     al_ustr_remove_chr(text.string, al_ustr_offset(text.string, -1));
@@ -115,13 +126,13 @@ public:
             } break;
         case ALLEGRO_EVENT_MOUSE_AXES:
             if (event.mouse.display == display) {
-                scroll += float(5 * event.mouse.dz);
+                text.offset += float(20 * event.mouse.dz);
             } break;
         }
     }
 
     void render() {
-        const float
+        float
             x1 = x - float(align.horizontal == Align::LEFT) * width
                 - float(align.horizontal == Align::CENTER) * width / 2.0f,
             y1 = y - float(align.vertical == Align::TOP) * height
@@ -130,12 +141,13 @@ public:
             y2 = y1 + height;
         if (fill.enabled) al_draw_filled_rounded_rectangle(x1, y1, x2, y2, rx, ry, fill.color);
         if (border.enabled) al_draw_rounded_rectangle(
-            x1 + border.offset, y1 - border.offset,
-            x2 + border.offset, y2 - border.offset,
+            x1 - border.padding - border.thickness + border.offset, y1 - border.padding - border.thickness + border.offset,
+            x2 + border.padding + border.thickness + border.offset, y2 + border.padding + border.thickness + border.offset,
             rx, ry, border.color, border.thickness);
-        draw_multiline_text(
-            x1 + padding, y1 + padding,
-            x2 - x1 - padding, y2 - y1 - padding);
+        x1 += text.padding; y1 += text.padding;
+        x2 -= text.padding; y2 -= text.padding;
+        if (text.multiline) draw_multiline_text(x1, y1, x2 - x1, y2 - y1);
+        else draw_text(x1, y1, x2 - x1, y2 - y1);
         al_draw_filled_rectangle(x - 5.0f, y - 5.0f, x + 5.0f, y + 5.0f, al_map_rgb(255,255,255));
     }
 
