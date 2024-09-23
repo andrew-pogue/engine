79a1de706e2084ea4d1476a873eb5a32c0d9ad2c
Andrew Pogue <andrewthomaspogue@gmail.com>
2024-09-23 15:14:46 -0700

    string

--- src/string.hh
+++ src/string.hh
@@ -2,8 +2,11 @@
 #include <allegro5/allegro.h>
 
 // wrapper class for ALLEGRO_USTR
-class String {
-public:
+struct String {
+
+    String(ALLEGRO_USTR *str)
+        : ustr_(str)
+    { assert(ustr_ && "[String] failed to create utf8 string"); }
 
     String(const char *str)
         : ustr_(al_ustr_new(str))
@@ -41,23 +44,23 @@ public:
 
     void operator+=(const String &other) { al_ustr_append(ustr_, other.ustr_); }
     void operator+=(const char *str) { al_ustr_append_cstr(ustr_, str); }
-    void operator+=(int unichar) { return al_ustr_append_chr(ustr_, unichar); }
+    void operator+=(int unichar) { al_ustr_append_chr(ustr_, unichar); }
 
     String substr(int i, int j) {
         return String(al_ustr_dup_substr(ustr_, al_ustr_offset(ustr_, i), al_ustr_offset(ustr_, j)));
     }
 
     bool insert(const String &other, int i) { return al_ustr_insert(ustr_, al_ustr_offset(ustr_, i), other.ustr_); }
-    bool insert(const char &str,     int i) { return al_ustr_insert_crst(ustr_, al_ustr_offset(ustr_, i), str); }
+    bool insert(const char *str,     int i) { return al_ustr_insert_cstr(ustr_, al_ustr_offset(ustr_, i), str); }
     bool insert(int unichar,         int i) { return al_ustr_insert_chr(ustr_, al_ustr_offset(ustr_, i), unichar); }
 
     bool append(const String &other) { return al_ustr_append(ustr_, other.ustr_); }
     bool append(const char *str) { return al_ustr_append_cstr(ustr_, str); }
     bool append(int unichar) { return al_ustr_append_chr(ustr_, unichar); }
-    bool append(const char &fmt, ...) {
-        val_list args;
+    bool append(const char *fmt, ...) {
+        va_list args;
         va_start(args, fmt);
-        bool ret = al_ustr_vappendf(ustr_, unichar);
+        bool ret = al_ustr_vappendf(ustr_, fmt, args);
         va_end(args);
         return ret;
     }
@@ -71,18 +74,43 @@ public:
     bool trim_whitespace_left() { return al_ustr_ltrim_ws(ustr_); }
     bool trim_whitespace_right() { return al_ustr_rtrim_ws(ustr_); }
 
-    void set_char(int i, int unichar) { return al_ustr_set_chr(ustr_, al_ustr_offset(ustr_, i),  unichar); }
+    void set_char(int i, int unichar) { al_ustr_set_chr(ustr_, al_ustr_offset(ustr_, i),  unichar); }
     int get_char(int i) const { return al_ustr_get(ustr_, al_ustr_offset(ustr_, i)); }
 
     bool replace(int i, int j, const String &str) {
-        return al_ustr_replace_range(ustr_, al_ustr_offset(ustr_, i), al_ustr_offset(ustr_, j), str.get());
+        return al_ustr_replace_range(ustr_, al_ustr_offset(ustr_, i), al_ustr_offset(ustr_, j), str);
     }
 
-    unsigned size() const { return al_ustr_length(ustr_); }
+    unsigned length() const { return al_ustr_length(ustr_); }
 
     const char *cstr() const { return al_cstr(ustr_); }
 
-    ALLEGRO_USTR *get() { return ustr_; }
+    operator       ALLEGRO_USTR *()       { return ustr_; }
+    operator const ALLEGRO_USTR *() const { return ustr_; }
+
+    struct ConstIterator {
+
+        ConstIterator(ALLEGRO_USTR *str, int offset) : str(str), pos(offset) {}
+
+        ConstIterator operator++() { al_ustr_next(str, &pos); return *this; }
+        ConstIterator operator--() { al_ustr_prev(str, &pos); return *this; }
+        ConstIterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+        ConstIterator operator--(int) { auto tmp = *this; --(*this); return tmp; }
+
+        bool operator==(const ConstIterator &other) const { return other.str == str && other.pos == pos; }
+        bool operator!=(const ConstIterator &other) const { return !(other == *this); }
+
+        int operator*() const { return al_ustr_get(str, pos); }
+
+    private:
+
+        ALLEGRO_USTR *str; // non-owning
+        int pos;
+
+    }; // struct ConstIterator
+
+    ConstIterator cbegin() const { return ConstIterator(ustr_, al_ustr_offset(ustr_, 0)); }
+    ConstIterator cend() const { return ConstIterator(ustr_, al_ustr_size(ustr_)); }
 
 private:
     
