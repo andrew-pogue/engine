eb891893e2e766192d8dcca197b23a5610c9f4c0
Andrew Pogue <andrewthomaspogue@gmail.com>
2024-09-23 15:09:10 -0700

    layout

--- src/layout.hh
+++ src/layout.hh
@@ -1,63 +1,142 @@
 #pragma once
-#include "align.hh"
-#include "panel.hh"
+#include <concepts>
+#include "vector2.hh"
+#include "rectangle.hh"
 
-void standard_align_layout(Widget &widget, Align align, float width, float height) {
-    switch(align.y) {
-    case ALIGN_CENTER: widget.y += (height - widget.height) / 2.0f; break;
-    case ALIGN_BOTTOM: widget.y += height - widget.height; break;
+enum : int {
+    ANCHOR_CENTER,
+    ANCHOR_NORTH, ANCHOR_SOUTH,
+    ANCHOR_EAST,  ANCHOR_WEST,
+    ANCHOR_NORTHEAST, ANCHOR_NORTHWEST,
+    ANCHOR_SOUTHEAST, ANCHOR_SOUTHWEST,
+};
+
+enum : int {
+    ALIGN_LEFT,
+	ALIGN_TOP = ALIGN_LEFT,
+	ALIGN_RIGHT,
+	ALIGN_BOTTOM = ALIGN_RIGHT,
+	ALIGN_CENTER,
+};
+
+struct GridLayout;
+
+struct Layout : Rectangle {
+
+    /*
+    GridLayout grid(int rows, int columns, Vector2<float> spacing={0.f, 0.f}) const {
+        return GridLayout(*this, rows, columns, spacing);
     }
-    switch(align.x) {
-    case ALIGN_CENTER: widget.x += (width - widget.width) / 2.0f; break;
-    case ALIGN_RIGHT: widget.x += width - widget.width; break;
+    */
+
+    void fill(std::derived_from<Rectangle> auto &...items) const {
+        for (Rectangle *item : { &items... }) *item = *(Rectangle *)this;
     }
-}
 
-struct Row : Panel {
-
-    Row(float x, float y, float w, float h, Align align)
-        : Panel(x, y, w, h)
-        , align(align)
-    {}
-
-    void layout() override {
-        float shift_x = 0.0f;
-        for (auto widget : widgets_) {
-            standard_align_layout(*widget, align, width, height);
-            switch (align.x) {
-            case ALIGN_LEFT: widget->x += shift_x; break;
-            case ALIGN_CENTER: widget->x -= shift_x / 2.0f; break;
-            case ALIGN_RIGHT: widget->x -= shift_x; break;
-            }
-            shift_x += widget->width;
+    void fill_horizontal(std::derived_from<Rectangle> auto &...items) const {
+        for (Rectangle *item : { &items... }) {
+            item->x = x;
+            item->width = width;
         }
     }
 
-    Align align;
-
-};
-
-struct Column : Panel {
-
-    Column(float x, float y, float w, float h, Align align)
-        : Panel(x, y, w, h)
-        , align(align)
-    {}
-
-    void layout() override {
-        float shift_y = 0.0f;
-        for (auto widget : widgets_) {
-            standard_align_layout(*widget, width, height);
-            switch (align.y) {
-            case ALIGN_TOP: widget->y += shift_y; break;
-            case ALIGN_CENTER: widget->y -= shift_y / 2.0f; break;
-            case ALIGN_BOTTOM: widget->y -= shift_y; break;
-            }
-            shift_y += widget->height;
+    void fill_vertical(std::derived_from<Rectangle> auto &...items) const {
+        for (Rectangle *item : { &items... }) {
+            item->y = y;
+            item->height = height;
         }
     }
 
-    Align align;
+    float row(float spacing, std::derived_from<Rectangle> auto &...items) const {
+        float shift = 0.0f;
+        for (Rectangle *item : { &items... }) {
+            item->x = x + shift;
+            shift += item->width + spacing;
+        }
+        return shift - spacing;
+    }
 
-};
+    float column(float spacing, std::derived_from<Rectangle> auto &...items) const {
+        float shift = 0.0f;
+        for (Rectangle *item : { &items... }) {
+            item->y = y + shift;
+            shift += item->height + spacing;
+        }
+        return shift - spacing;
+    }
+
+    void align_horizontal(char value, std::derived_from<Rectangle> auto &...items) const {
+        for (Rectangle *item : { &items... }) {
+            switch (value) {
+            case ALIGN_LEFT:   break;
+            case ALIGN_CENTER: item->x = x + (width - item->width) / 2.0f; break;
+            case ALIGN_RIGHT:  item->x = x +  width - item->width; break;
+            }
+        }
+    }
+
+    void align_vertical(char value, std::derived_from<Rectangle> auto &...items) const {
+        for (Rectangle *item : { &items... }) {
+            switch (value) {
+            case ALIGN_TOP:    break;
+            case ALIGN_CENTER: item->y = y + (height - item->height) / 2.0f; break;
+            case ALIGN_BOTTOM: item->y = y +  height - item->height; break;
+            }
+        }
+    }
+
+    void align(char xalign, char yalign, std::derived_from<Rectangle> auto &...items) const {
+        align_horizontal(xalign, items...);
+        align_vertical(yalign, items...);
+    }
+
+    void anchor(char value, std::derived_from<Rectangle> auto &...items) const {
+        switch (value) {
+        case ANCHOR_NORTH:     align(ALIGN_CENTER, ALIGN_TOP,    items...); break;
+        case ANCHOR_NORTHEAST: align(ALIGN_RIGHT,  ALIGN_TOP,    items...); break; 
+        case ANCHOR_NORTHWEST: align(ALIGN_LEFT,   ALIGN_TOP,    items...); break;
+        case ANCHOR_SOUTH:     align(ALIGN_CENTER, ALIGN_BOTTOM, items...); break;
+        case ANCHOR_SOUTHEAST: align(ALIGN_RIGHT,  ALIGN_BOTTOM, items...); break;
+        case ANCHOR_SOUTHWEST: align(ALIGN_LEFT,   ALIGN_BOTTOM, items...); break;
+        case ANCHOR_WEST:      align(ALIGN_LEFT,   ALIGN_CENTER, items...); break;
+        case ANCHOR_EAST:      align(ALIGN_RIGHT,  ALIGN_CENTER, items...); break;
+        case ANCHOR_CENTER:    align(ALIGN_CENTER, ALIGN_CENTER, items...); break;
+        }
+    }
+
+}; // struct Layout
+
+struct GridLayout : Rectangle {
+
+    int rows, columns;
+    Vector2<float> spacing;
+
+    GridLayout(Rectangle area, int columns, int rows, Vector2<float> spacing={0.f, 0.f})
+        : Rectangle(area)
+        , rows(rows), columns(columns)
+        , spacing(spacing)
+    {}
+
+    Layout at(int gx, int gy) const {
+        const float tw = (width - (columns + 1) * spacing.x) / columns;
+        const float th = (height - (rows + 1) * spacing.y) / rows;
+        return {
+            x + spacing.x + float(gx) * (tw + spacing.x),
+            y + spacing.y + float(gy) * (th + spacing.y),
+            tw , th
+        };
+    }
+
+    Layout area(int gx, int gy, int gw, int gh) const {
+        const float tw = (width - (columns + 1) * spacing.x) / columns;
+        const float th = (height - (rows + 1) * spacing.y) / rows;
+        return {
+            x + spacing.x + float(gx) * (tw + spacing.x),
+            y + spacing.y + float(gy) * (th + spacing.y),
+            float(gw) * (tw + spacing.x) - spacing.x,
+            float(gh) * (th + spacing.y) - spacing.y
+        };
+    }
+
+}; // struct GridLayout
 
--- src/widget.hh
+++ src/widget.hh
@@ -1,21 +1,21 @@
 #pragma once
+#include <allegro5/allegro.h>
+#include "rectangle.hh"
 
 // a graphical user-interface element
 struct Widget {
 
-    float preferred_width, preferred_height;
+    float x, y, width, height;
 
-    Widget(float width, float height)
-        : preferred_width(width), preferred_height(height)
-    {}
+    Widget(Rectangle area) : x{area.x}, y{area.y}, width{area.width}, height{area.height} { }
+    Widget(float x, float y, float w, float h) : x{x}, y{y}, width{w}, height{h} { }
 
-    virtual ~Widget() {}
-
-    // returns true if event was handled
-    virtual bool handle_event(const ALLEGRO_EVENT &event) { return false };
+    virtual void handle_event(const ALLEGRO_EVENT &event) { }
     // returns false if exit condition was met
-    virtual bool update(double dt) { return true; };
-    virtual void render(float x, float y) const = 0;
+    virtual bool update(double dt) { return true; }
+    virtual void render() const { }
+
+    Rectangle area() const { return { x, y, width, height }; }
 
 };
 
